7d4d84a1f94fcf21b5ca5177c7861ed105ba481c
Peterson <petersonsimiao1@gmail.com>
2021-08-11 00:23:07 -0300

    feat(Demo): Importando lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgxIuguService } from 'projects/ngx-iugu/src/public-api';
 
 @Component({
   selector: 'dd-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dd-iugu';
+
+  constructor(private IuguService: NgxIuguService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxIuguModule } from 'projects/ngx-iugu/src/public-api';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxIuguModule.footRoot({
+      accountID: environment.accountID,
+      CDN: environment.CDN,
+      testMode: !environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
