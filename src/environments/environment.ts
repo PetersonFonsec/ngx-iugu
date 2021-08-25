88a538966d473312e6f0e76f6e42d2768b115aeb
Peterson <petersonsimiao1@gmail.com>
2021-08-24 23:20:54 -0300

    feat(demo): criado novos campos no environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  accountID: '',
+  CDN: 'https://js.iugu.com/v2',
 };
 
 /*
