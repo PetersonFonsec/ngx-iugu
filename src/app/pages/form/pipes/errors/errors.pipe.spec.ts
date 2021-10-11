ea620ad854a4375cebde026b0e99210ac4d83756
Peterson <petersonsimiao1@gmail.com>
2021-10-11 15:33:06 -0300

    chore(demo): arquivo de teste unitarios do pipe de erro

---
+++ src/app/pages/form/pipes/errors/errors.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ErrorsPipe } from './errors.pipe';
+
+describe('ErrorsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ErrorsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
